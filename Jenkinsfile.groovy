4696fb425e85dd24522ce2cb0d3b91b050a92b66
khoaviper1 <121345931+khoaviper1@users.noreply.github.com>
2024-05-28 02:50:33 +0700

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,16 +3,13 @@ pipeline {
 
     environment {
         EC2_HOST = '34.207.58.41'  // Địa chỉ IP của máy EC2
-        SSH_KEY = credentials('19521698')  // Thay 'ec2-ssh' bằng ID của khóa SSH được lưu trong Jenkins Credentials
     }
-
-    stages {
         stage('Deploy to EC2') {
             steps {
                 // Bước này để triển khai mã nguồn lên máy EC2
                 script {
                     sshagent(['19521698']) {
-                        sh "scp -i ${SSH_KEY} -r * ubuntu@${EC2_HOST}:/var/www/html/"
+                        sh "scp -o StrictHostKeyChecking=no -r * ubuntu@${EC2_HOST}:/var/www/html/"
                     }
                 }
             }
