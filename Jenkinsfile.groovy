5e4e83d6d65ccd192f77c868bb45a8ee1533c25d
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2024-05-30 01:36:45 +0700

    Create Jenkinsfile.groovy

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent any
+
+    environment {
+        EC2_HOST = '100.26.166.63'  // Địa chỉ IP của máy EC2
+    }
+
+    stages {
+        stage('Deploy to EC2') {
+            steps {
+                // Bước này để triển khai mã nguồn lên máy EC2
+                script {
+                    sshagent(['19521698']) {
+                        sh "scp -o StrictHostKeyChecking=no -r * ubuntu@${EC2_HOST}:/var/www/html/"
+                    }
+                }
+            }
+        }
+    }
+}
